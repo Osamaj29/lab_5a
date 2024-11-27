7f7951d46620f4a70d2feecea0a9629a61fcb1f3
Osamaj29 <osama0557562398@gmail.com>
2024-11-27 16:43:02 +0800

    Add files via upload

---
+++ lab5a_q1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+</head>
+<body>
+    <?php 
+        $name = "Osama Jallad";
+        $matricNumber = "Bi220032";
+        $course = "Information Security";
+        $year = "2nd Year";
+        $address = "Batu Pahat/Jalan University";
+    ?>
+
+    <table border="1">
+        <tr>
+            <th>Detail</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo $matricNumber; ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo $course; ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo $year; ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $address; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
